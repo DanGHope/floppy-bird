293222cf6c1f068e32c43a1edf35a658574d17f6
Dan Hope <dan.g.hope@gmail.com>
2016-03-30 18:50:28 -0400

    adding basic files

--- main.js
+++ main.js
@@ -8,31 +8,89 @@ var renderer = new PIXI.autoDetectRenderer(400, 300);
 var pixiDiv = document.getElementById('pixi');
 console.log(pixiDiv);
 pixiDiv.appendChild(renderer.view);
+renderer.backgroundColor = 0x111111;
 
 // You need to create a root container that will hold the scene you want to draw.
 var stage = new PIXI.Container();
 
 // image loader
 var loader = new PIXI.loaders.Loader();
-loader.add('bird','./objects/bird.js');
-
+loader.add('bird1', '../images/bird-1.png');
+loader.add('bird2', '../images/bird-2.png');
+//loading message
+loader.on('loading',console.log('Loading'));
 //once items are loaded start
-loader.once('complete',animate);
+loader.once('complete', init);
 loader.load();
 
-function logic(){
+//time variables
+var lasttime;
+var currtime;
+var delta;
 
+//bird object
+//create bird
+var bird;
+//frame images
+var FRAMES = [
+  '../images/bird-1.png',
+  '../images/bird-2.png'
+];
+//current frame
+var frameindex;
+//frame animation time
+var frametime;
+//framerate
+var FRAMERATE = 0.08;
+//move speed
+var VELOCITY = 100;
 
+function init() {
+  console.log('init');
+
+  bird = new PIXI.Sprite(PIXI.Texture.fromFrame(FRAMES[0]));
+  frameindex = 0;
+  frametime = FRAMERATE;
+  bird.anchor.x = 0.5;
+  bird.anchor.y = 0.5;
+  bird.position.y = 100;
+  bird.position.x = 100;
+  bird.scale.x = 0.1;
+  bird.scale.y = 0.1;
+
+  //add bird to stage;
+  stage.addChild(bird);
+
+  //start animation
+  lasttime = new Date().getMilliseconds();
+  animate();
+}
+
+function logic(delta) {
+  //move bird
+  bird.position.x += (VELOCITY * delta);
+
+  //animate bird
+  frametime -= delta;
+  if (frametime <= 0) {
+    frameindex++;
+    if (frameindex >= FRAMES.length) {
+      frameindex = 0;
+    }
+    bird.texture = PIXI.Texture.fromFrame(FRAMES[frameindex]);
+    frametime = FRAMERATE;
+  }
 }
 
 function animate() {
   // start the timer for the next animation loop
-  requestAnimationFrame(animate);
+  currtime = new Date().getMilliseconds();
+  delta = (currtime - lasttime) / 1000;
 
-  logic();
+  logic(delta);
 
-
-
-  // this is the main render call that makes pixi draw your container and its children.
+  //render and update animation/time
   renderer.render(stage);
+  requestAnimationFrame(animate);
+  lasttime = currtime;
 }
